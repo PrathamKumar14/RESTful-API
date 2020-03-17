50bf6d79e764e571288ccbca68a86b2f1d63e42b
Pratham kumar <prathamsolera@gmail.com>
2020-03-17 15:50:22 +0530

    Start Server

---
+++ app.js
@@ -0,0 +1,22 @@
+// jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static("public"));
+
+
+
+app.listen(3000, function(){
+  console.log("server started at port 3000");
+});
