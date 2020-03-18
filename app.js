d98f1bcb6a25bd3775fc9161ee0ae3fdb280139a
Pratham kumar <prathamsolera@gmail.com>
2020-03-19 01:35:26 +0530

    POST request: Create one new article

--- app.js
+++ app.js
@@ -35,6 +35,22 @@ app.get("/articles", function(req, res){
   });
 });
 
+app.post("/articles", function(req, res){
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  newArticle.save(function(err){
+    if(!err){
+      res.send("Successful");
+    }
+    else{
+      res.send(err);
+    }
+  });
+});
+
 app.listen(3000, function(){
   console.log("server started at port 3000");
 });
