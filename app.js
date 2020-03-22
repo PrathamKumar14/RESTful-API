56e4f6cf312455400cbf1bf3b815795f8fc74047
Pratham kumar <prathamsolera@gmail.com>
2020-03-22 10:49:33 +0530

    Chained route methods

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const articleSchema = {
   title: String,
@@ -24,44 +27,40 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function(req, res){
-  Article.find({}, function(err, foundArticles){
-    if(!err){
+app.route("/articles")
+.get(function(req, res) {
+  Article.find({}, function(err, foundArticles) {
+    if (!err) {
       res.send(foundArticles);
-    }
-    else{
+    } else {
       res.send(err);
     }
   });
-});
-
-app.post("/articles", function(req, res){
+})
+.post(function(req, res) {
   const newArticle = new Article({
     title: req.body.title,
     content: req.body.content
   });
 
-  newArticle.save(function(err){
-    if(!err){
+  newArticle.save(function(err) {
+    if (!err) {
       res.send("Successful");
-    }
-    else{
+    } else {
       res.send(err);
     }
   });
-});
-
-app.delete("/articles", function(req, res){
-  Article.deleteMany(function(err){
-    if(!err){
+})
+.delete(function(req, res) {
+  Article.deleteMany(function(err) {
+    if (!err) {
       res.send("Successfully deleted all articles");
-    }
-    else{
+    } else {
       res.send(err);
     }
   });
 });
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("server started at port 3000");
 });
