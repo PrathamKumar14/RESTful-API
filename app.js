a763426f96cea5ac9501d7ca37cccdd3f0c87af6
Pratham kumar <prathamsolera@gmail.com>
2020-03-24 01:29:29 +0530

    Delete request: Delete all article on particular article

--- app.js
+++ app.js
@@ -101,6 +101,20 @@ app.route("/articles/:articleTitle")
       }
     }
   );
+})
+
+.delete(function(req, res){
+  Article.deleteOne(
+    {title: req.params.articleTitle},
+    function(err){
+      if(!err){
+        res.send("Successfully deleted");
+      }
+      else {
+        res.send(err);
+      }
+    }
+  );
 });
 
 app.listen(3000, function() {
